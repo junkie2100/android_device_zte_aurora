228a43dbf700f1f56335274e361034c45599bd8b
Dm47021 <dm47021@hotmail.com>
2013-11-01 00:57:59 -0400

    Update for Prima WLAN

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -101,21 +101,18 @@ ENABLE_WEBGL := true
 TARGET_FORCE_CPU_UPLOAD := true
 WEBCORE_INPAGE_VIDEO := true
 
-# Wifi
-# HOSTAPD-derived wifi driver
+# Wifi Qcom Prima Wlan 
 BOARD_HAS_QCOM_WLAN := true
 BOARD_WLAN_DEVICE := qcwcn
-BOARD_SOFTAP_DEVICE := qcwcn 
-WPA_SUPPLICANT_VERSION := VER_0_8_X
 BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-WIFI_DRIVER_MODULE_NAME :=  wlan
-WIFI_DRIVER_MODULE_PATH :=  "/system/lib/modules/wlan.ko"
-WIFI_DRIVER_FW_PATH_AP := "ap"
-WIFI_DRIVER_FW_PATH_STA := "sta"
 BOARD_HOSTAPD_DRIVER := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-###
+WIFI_DRIVER_FW_PATH_AP := "ap"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WIFI_DRIVER_MODULE_NAME := prima_wlan
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/prima_wlan.ko"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 loglevel=7 kgsl.mmutype=gpummu
